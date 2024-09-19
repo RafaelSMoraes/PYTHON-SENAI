fa648a62025ffe5c32f4d2dcb4f6df8daa3923f1
Rafael Moraes <138817803+RafaelSMoraes@users.noreply.github.com>
2024-09-19 16:00:01 -0300

    Update 025.py

--- 025.py
+++ 025.py
@@ -1,9 +1,68 @@
-#Crie um programa para jogar JOKEMPO, usando a função random.randint
-
+# Crie um programa para jogar JOKEMPO, usando a função random.randint
 import random
+import time
 
-p1 = 'Pedra'
-p2 = 'Papel'
-p3 = 'Tesoura'
+'''
 
+#jogador
 
+jogador = int(input('Digite um número de 1 a 3: '))
+if jogador == 1:
+    print('Você escolheu pedra')
+elif jogador == 2:
+    print('Você escolheu papel')
+elif jogador == 3:
+    print('Você escolheu tesoura')
+
+#maquina
+
+maquina = random.randint(1, 3)
+if maquina == 1:
+    print('Maquina escolheu pedra')
+elif maquina == 2:
+    print('Maquina escolheu papel')
+elif maquina == 3:
+    print('Maquina escolheu tesoura')
+
+#jogo
+
+if maquina == jogador:
+    print('Empate')
+elif maquina == 1 and jogador == 3:
+    print('Vitória da maquina')
+elif maquina == 2 and jogador == 1:
+    print('Vitória da maquina')
+elif maquina == 3 and jogador == 2:
+    print('Vitória da maquina')
+else:
+    print('Vitória do jogador')
+
+'''
+
+pc = random.randint(1, 3)
+j = int(input('Digite a sua escolha: '
+              '\n1- Pedra'
+              '\n2- Papel'
+              '\n3- Tesoura'
+              '\n  ----------------> '))
+
+print('jo')
+time.sleep(1)
+print('kem')
+time.sleep(1)
+print('PO')
+time.sleep(1)
+print('... quase lá')
+if j == pc:
+    print('empate')
+elif ((j == 1 and pc == 3) or
+      (j == 2 and pc == 1) or
+      (j == 3 and pc == 2)):
+    print('Você Ganhou!!')
+else:
+    print('Você perdeu!')
+
+if j == pc:
+    print('empate')
+elif j == 1 and pc == 2:
+    print('Computador vence ')
