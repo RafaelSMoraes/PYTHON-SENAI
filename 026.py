a0a8b4fc34b659697c1698dd45c8af6125d586aa
Rafael Moraes <138817803+RafaelSMoraes@users.noreply.github.com>
2024-09-19 16:00:29 -0300

    Update 026.py

--- 026.py
+++ 026.py
@@ -1,10 +1,12 @@
-#Crie um programa para analisar o IMC de uma pessoa,
-# e classifique se ela está entre a faixa ideal, acima ou abaixo do IMC ideal.
+#Crie um programa para analisar o IMC de uma pessoa, e classifique se ela está entre a faixa ideal, acima ou abaixo do IMC ideal.
 
-altura = float(input('Altura: '))
-peso = float(input('Peso: '))
+altura = float(input('Digite sua altura: '))
+peso = float(input('Digite seu peso: '))
+imc = peso / (altura**2)
 
-imc = (altura / peso**2)
-
-if imc < 16:
-    print('Baixo')
+if imc > 25:
+    print(f'IMC de {imc} você está sobre o seu peso ideal')
+elif imc > 18.5:
+    print(f'IMC de {imc} você está com peso ideal')
+else:
+    print(f'IMC de {imc} está abaixo do ideal')
