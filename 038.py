e97f2aee7656b42305e7518fb94df7ad32e1b777
Rafael Moraes <138817803+RafaelSMoraes@users.noreply.github.com>
2024-10-10 20:37:49 -0300

    Update 038.py
    Inclusão das opções 3 e 4.
    Adição da variável 'numero' que recebe uma lista.

--- 038.py
+++ 038.py
@@ -12,6 +12,7 @@ import random
 
 opcao = ''
 while opcao != 'Sair':
+    numero = []
     for i in range (3):
         n1 = int(input('Numero: '))
         n2 = int(input('Numero: '))
@@ -23,10 +24,19 @@ while opcao != 'Sair':
                   '\n3 - Maior'
                   '\n4 - Novos numeros'
                   '\n5 - Sair ----> ')
+
         if opcao == '1':
             print(f'A soma  é {n1 + n2 + n3}')
         elif opcao == '2':
             print(f'A multiplicação é {n1 * n2 * n3}')
+        elif opcao == '3':
+            maior_numero = numero[0] #começa pelo primeiro elemento
+            for num in numero [1:]: #começa da segunda posição
+                if num > maior_numero:
+                    maior_numero = num
+            print(f'O maior número é {maior_numero}')
+        elif opcao == '4':
+            print('Digite novos numeros: ')
         else:
-            print('erro')
+            print('Você saiu')
             exit()
