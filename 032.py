ae7b6e35e5445719f75ebb90a4341a01a36c5975
Rafael Moraes <138817803+RafaelSMoraes@users.noreply.github.com>
2024-11-22 19:12:24 -0300

    Update 032.py

--- 032.py
+++ 032.py
@@ -17,15 +17,17 @@ for ele in range (n2, n1)[:: -1]:
 
 '''
 #metodo professor
+try:
+    inicio = int(input('Inicio: '))
+    fim = int(input('Fim: '))
 
-inicio = int(input('Inicio: '))
-fim = int(input('Fim: '))
-
-if inicio < fim:
-    for i in range(inicio, fim):
-        if i % 2 == 0:
-            print(i)
-else:
-    for i in range(inicio, fim -1):
-        if i % 2 == 0:
-            print(i)
+    if inicio < fim:
+        for i in range(inicio, fim):
+            if i % 2 == 0:
+                print(i)
+    else:
+        for i in range(inicio, fim -1):
+            if i % 2 == 0:
+                print(i)
+except ValueError:
+    print('Apenas numeros')
