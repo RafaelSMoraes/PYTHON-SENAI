3ba37905189490321b0d938a7261ddecbe16db16
Rafael Moraes <138817803+RafaelSMoraes@users.noreply.github.com>
2024-11-22 19:02:49 -0300

    Update 006.py

--- 006.py
+++ 006.py
@@ -1,13 +1,18 @@
 #Escreva um programa que leia 6 notas diferentes e faça a média do aluno
 #Saída esperada: A sua média final é : 5
 
-nota1 = float(input('Coloque a nota do Aluno'))
-nota2 = float(input('Coloque a nota do Aluno'))
-nota3 = float(input('Coloque a nota do Aluno'))
-nota4 = float(input('Coloque a nota do Aluno'))
-nota5 = float(input('Coloque a nota do Aluno'))
-nota6 = float(input('Coloque a nota do Aluno'))
 
-media_final = (nota1 + nota2 + nota3 + nota4 + nota5 + nota6) / 6
+try:
+    nota1 = float(input('Coloque a nota do Aluno'))
+    nota2 = float(input('Coloque a nota do Aluno'))
+    nota3 = float(input('Coloque a nota do Aluno'))
+    nota4 = float(input('Coloque a nota do Aluno'))
+    nota5 = float(input('Coloque a nota do Aluno'))
+    nota6 = float(input('Coloque a nota do Aluno'))
 
-print(f'A sua media final é: {media_final}')
+    media_final = (nota1 + nota2 + nota3 + nota4 + nota5 + nota6) / 6
+
+    print(f'A sua media final é: {media_final}')
+
+except ValueError:
+    print('Apenas numeros')
