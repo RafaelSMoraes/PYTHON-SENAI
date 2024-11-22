3013b198768a534e71ec0bca5863c835de523f8e
Rafael Moraes <138817803+RafaelSMoraes@users.noreply.github.com>
2024-11-22 19:05:10 -0300

    Update 015.py

--- 015.py
+++ 015.py
@@ -1,8 +1,12 @@
 #Escreva um programa que peça ao usuário um número e imprima se é par ou ímpar
 
-n1 = float(input('Coloque um numero para saber se ele é par ou impar: '))
+try:
+    n1 = float(input('Coloque um numero para saber se ele é par ou impar: '))
 
-if n1 % 2 == 0:
-    print('o numero é par')
-else:
-    print('o numero é impar')
+    if n1 % 2 == 0:
+        print('o numero é par')
+    else:
+        print('o numero é impar')
+
+except ValueError:
+    print('Apenas numeros')
