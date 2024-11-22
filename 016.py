d7f54646fbab26a3df6e4989053da0e4b506e067
Rafael Moraes <138817803+RafaelSMoraes@users.noreply.github.com>
2024-11-22 19:05:24 -0300

    Update 016.py

--- 016.py
+++ 016.py
@@ -1,9 +1,12 @@
 #Escreva um programa que peça ao usuário dois números e imprima se eles são iguais ou diferentes.
+try:
+    n1 = float(input('Insira um numero:'))
+    n2 = float(input('Insira outro numero: '))
 
-n1 = float(input('Insira um numero:'))
-n2 = float(input('Insira outro numero: '))
+    if n1 == n2:
+        print(f'{n1} é igual a {n2}')
+    else:
+        print(f'{n1} é diferente de {n2}')
 
-if n1 == n2:
-    print(f'{n1} é igual a {n2}')
-else:
-    print(f'{n1} é diferente de {n2}')
+except ValueError:
+    print('Apenas numeros')
