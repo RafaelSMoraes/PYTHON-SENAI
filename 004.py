85c796297bb2d0791b2c8f09495f9537c3bf065f
Rafael Moraes <138817803+RafaelSMoraes@users.noreply.github.com>
2024-11-22 19:02:04 -0300

    Update 004.py

--- 004.py
+++ 004.py
@@ -3,12 +3,16 @@
 #V = (4/3) . π . r³
 #A = 4 . π . r²
 
-#leitura de dados
-raio = float(input('digite o raio:'))
+try:
+    #leitura de dados
+    raio = float(input('digite o raio:'))
 
-#calculo de volume e area
-volume = (4 / 3) * 3.14 * (raio**3)
-area = 4* 3.14 * (raio**2)
+    #calculo de volume e area
+    volume = (4 / 3) * 3.14 * (raio**3)
+    area = 4* 3.14 * (raio**2)
 
-#saida de dados
-print(f'o volume da esfera é: {volume: .2f}, e a área é de {area: .2f}')
+    #saida de dados
+    print(f'o volume da esfera é: {volume: .2f}, e a área é de {area: .2f}')
+
+except ValueError:
+    print('Apenas numeros')
