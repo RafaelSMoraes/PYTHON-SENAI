d21f169b5c5d1d448312db12e2b2fbc1e93ca592
Rafael Moraes <138817803+RafaelSMoraes@users.noreply.github.com>
2024-11-22 19:03:52 -0300

    Update 008.py

--- 008.py
+++ 008.py
@@ -1,8 +1,12 @@
 #Crie um algoritmo que leia um salário e simule um reajuste positivo de 60%.
 #Saida esperada: O salário de 6000 com o reajuste de 60% será de : 9600
 
-salario = float(input('Insira o Salario: '))
-reajuste = salario * 0.60
-valor_final = salario + reajuste
+try:
+    salario = float(input('Insira o Salario: '))
+    reajuste = salario * 0.60
+    valor_final = salario + reajuste
 
-print(f'O salario de {salario} com o reajuste de 60% será de: {valor_final}')
+    print(f'O salario de {salario} com o reajuste de 60% será de: {valor_final}')
+
+except ValueError:
+    print('Apenas numeros')
