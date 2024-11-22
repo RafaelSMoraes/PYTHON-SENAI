9e6c36be7f72538c0e9a5548fc705c0763623620
Rafael Moraes <138817803+RafaelSMoraes@users.noreply.github.com>
2024-11-22 19:07:34 -0300

    Update 022.py

--- 022.py
+++ 022.py
@@ -6,20 +6,23 @@ suficiente (entre 6 e 7)
 , bom (entre 7 e 9)
 ou excelente (9 ou maior).
 '''
+try:
+    n1 = int(input('Nota 1: '))
+    n2 = int(input('Nota 2: '))
+    n3 = int(input('Nota 3: '))
+    n4 = int(input('Nota 4: '))
+    n5 = int(input('Nota 5: '))
 
-n1 = int(input('Nota 1: '))
-n2 = int(input('Nota 2: '))
-n3 = int(input('Nota 3: '))
-n4 = int(input('Nota 4: '))
-n5 = int(input('Nota 5: '))
+    media = (n1 + n2 + n3 + n4 + n5) / 5
 
-media = (n1 + n2 + n3 + n4 + n5) / 5
+    if media >= 9 :
+        print('Excelente!.')
+    elif media >= 8 :
+        print('Bom.')
+    elif media >= 6 :
+        print('Suficiente.')
+    else:
+        print('Insuficiente')
 
-if media >= 9 :
-    print('Excelente!.')
-elif media >= 8 :
-    print('Bom.')
-elif media >= 6 :
-    print('Suficiente.')
-else:
-    print('Insuficiente')
+except ValueError:
+    print('Apenas numeros')
