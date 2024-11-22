72ffd951cd356498dfe01c027747a6c76cc32de2
Rafael Moraes <138817803+RafaelSMoraes@users.noreply.github.com>
2024-11-22 19:05:46 -0300

    Update 018.py

--- 018.py
+++ 018.py
@@ -1,8 +1,10 @@
 #Escreva um programa que peça ao usuário uma idade e imprima se é maior ou menor de idade (18 anos).
+try:
+    idade = int(input('Insira a sua idade: '))
 
-idade = int(input('Insira a sua idade: '))
-
-if idade >= 18:
-    print('Tá liberado, pode beber')
-else:
-    print('De menor não entra aqui não man.')
+    if idade >= 18:
+        print('Tá liberado, pode beber')
+    else:
+        print('De menor não entra aqui não man.')
+except ValueError:
+    print('Apenas numeros')
