75eb737c39c9a57e6219dc41f91b603ebeca0658
Rafael Moraes <138817803+RafaelSMoraes@users.noreply.github.com>
2024-11-22 19:01:36 -0300

    Update 002.py

--- 002.py
+++ 002.py
@@ -1,7 +1,9 @@
 #Escreva um programa que leia, o nome, idade, e cidade de nascimento e retorne para o usuário
+try:
+    nome = input('Digite seu nome: ')
+    idade = int(input('Digite sua idade: '))
+    cidade_nascimento = input('Digite sua cidade de nascimento: ')
 
-nome = input('Digite seu nome: ')
-idade = int(input('Digite sua idade: '))
-cidade_nascimento = input('Digite sua cidade de nascimento: ')
-
-print(f'Meu nome é {nome}, tenho {idade} anos e nasci em {cidade_nascimento}')
+    print(f'Meu nome é {nome}, tenho {idade} anos e nasci em {cidade_nascimento}')
+except ValueError:
+    print('Apenas numeros')
