560c21f23b899b0fb709420985d55167b74add64
Rafael Moraes <138817803+RafaelSMoraes@users.noreply.github.com>
2024-11-22 19:04:24 -0300

    Update 014.py

--- 014.py
+++ 014.py
@@ -1,10 +1,14 @@
 #Escreva um programa que peça ao usuário um número e imprima se é positivo ou negativo
 
-n1 = float(input('Digite um numero para saber se ele é positivo ou negativo: '))
+try:
+    n1 = float(input('Digite um numero para saber se ele é positivo ou negativo: '))
 
-if n1 > 0:
-    print('o numero é positivo')
-elif n1 == 0:
-    print('neutro')
-else:
-    print('o numero é negativo')
+    if n1 > 0:
+        print('o numero é positivo')
+    elif n1 == 0:
+        print('neutro')
+    else:
+        print('o numero é negativo')
+
+except ValueError:
+    print('Apenas numeros')
