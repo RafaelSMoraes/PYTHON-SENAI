bf028b482f85d7db159c55f623e5b81283b5c2d3
Rafael Moraes <138817803+RafaelSMoraes@users.noreply.github.com>
2024-11-22 19:24:23 -0300

    Update 037.py

--- 037.py
+++ 037.py
@@ -3,17 +3,20 @@
 # E no final, retorne também a quantidade de tentativas necessárias.
 
 import random
+try:
+    maquina = random.randint(1, 10)
+    print(maquina)
 
-maquina = random.randint(1, 10)
-print(maquina)
 
+    escolha = ''
+    tentativas = 0
+    while escolha != maquina:
+        escolha = int(input('Descubra o numero que eu estou pensando: '))
+        tentativas += 1
+        if escolha == maquina:
+            print(f'Voce levou {tentativas} tentativas para acertar e acertou, PARABENS!!')
+        else:
+            print('Voce errou')
 
-escolha = ''
-tentativas = 0
-while escolha != maquina:
-    escolha = int(input('Descubra o numero que eu estou pensando: '))
-    tentativas += 1
-    if escolha == maquina:
-        print(f'Voce levou {tentativas} tentativas para acertar e acertou, PARABENS!!')
-    else:
-        print('Voce errou')
+except ValueError:
+    print('Apenas numeros')
