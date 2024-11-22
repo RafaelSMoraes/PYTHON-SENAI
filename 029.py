832314f5138d8fb7ab076021571f75e428ff135d
Rafael Moraes <138817803+RafaelSMoraes@users.noreply.github.com>
2024-11-22 19:11:51 -0300

    Update 029.py

--- 029.py
+++ 029.py
@@ -1,7 +1,10 @@
 #Escreva um programa que imprima a tabuada de um número fornecido pelo usuário.
+try:
+    numeros = int(input('Digite um número para ter a tabuada dele: '))
 
-numeros = int(input('Digite um número para ter a tabuada dele: '))
+    for ele in range (0, 11):
+        neymar = ele * numeros
+        print(f'| {ele} * {numeros} = {neymar}|')
 
-for ele in range (0, 11):
-    neymar = ele * numeros
-    print(f'{ele} * {numeros} = {neymar}')
+except ValueError:
+    print('Apenas numeros')
