062dd0be75068a4e9a9a26adfc888f876a5d82d2
Rafael Moraes <138817803+RafaelSMoraes@users.noreply.github.com>
2024-11-23 12:43:33 -0300

    atualizacao 051.py

--- 051.py
+++ 051.py
@@ -4,13 +4,16 @@ Crie uma função para verificar se um número é par ou ímpar
 
 def par_ou_impar(numero):
     if numero % 2 == 0:
-        return 'Par' #usando o return para ficar mais "elegante" haha
+        return 'Par'
     else:
-        return 'impar'
+        return 'Impar'
 
+try:
 
-#utilização
+    #utilizacao
+    numero = int(input('Digite um numero: '))
+    resultado = par_ou_impar(numero)
+    print(f'O numero {numero} é {resultado}')
 
-numero = int(input('Digite um numero: '))
-resultado = par_ou_impar(numero)
-print(f'f O numero {numero} é {resultado}')
+except ValueError:
+    print('Inclua apenas números')
